5e66558e2abc770079a25dba73678eefd482304e
winner00 <toverturf@gmail.com>
2013-07-25 20:54:24 -0600

    Build - carbonize the bacon
    Change-Id: If703b8f0a2627fa1477a98090b07ad05c6136f80
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -521,7 +521,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka carbon
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
