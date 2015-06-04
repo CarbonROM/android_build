04a5d281ec54ec5c14d89d8eb3bad39638c3171e
Chris Sarbora <sarbs@cyngn.com>
2015-06-03 17:25:30 -0700

    Allow dopush to load binaries onto the "wrong" device
    * Lets solitary projects be built+pushed quickly even without a full
    build
    * Affects mmp, mmmp, mkap as well
    
    Change-Id: I494ead37988529e4b88be9590c7fc45b897ec8b7

--- envsetup.sh
+++ envsetup.sh
@@ -2382,7 +2382,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD") || [ "$FORCE_PUSH" == "true" ];
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
