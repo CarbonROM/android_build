069ca7b7fbb5fde8a1a7cb496758f4fb7b5c92a9
Steve Kondik <steve@cyngn.com>
2015-07-10 02:31:24 -0700

    build: Fix device detection for a few of the envsetup commands
     * Edge case where a certain developer ended up with a blank system
       partition in recovery. Wonder how that happened?
    
    Change-Id: I17854a53bf18d07c98c4b7f53df252d6f1d7113d
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -725,7 +725,7 @@ function eat()
             done
             echo "Device Found.."
         fi
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
+    if (adb shell getprop ro.carbon.device | grep -q "$CARBON_BUILD");
     then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
@@ -2015,7 +2015,7 @@ function installboot()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 > /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
+    if (adb shell getprop ro.carbon.device | grep -q "$CARBON_BUILD");
     then
         adb push $OUT/boot.img /cache/
         for i in $OUT/system/lib/modules/*;
@@ -2060,7 +2060,7 @@ function installrecovery()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 >> /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD");
+    if (adb shell getprop ro.carbon.device | grep -q "$CARBON_BUILD");
     then
         adb push $OUT/recovery.img /cache/
         adb shell dd if=/cache/recovery.img of=$PARTITION
@@ -2390,7 +2390,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell cat /system/build.prop | grep -q "ro.carbon.device=$CARBON_BUILD") || [ "$FORCE_PUSH" == "true" ];
+    if (adb shell getprop ro.carbon.device | grep -q "$CARBON_BUILD") || [ "$FORCE_PUSH" == "true" ];
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
