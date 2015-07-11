4695f94ad28c2f6cbd6698bbe83c17261bb66570
Myself5 <myself5@carbonrom.org>
2015-07-12 00:02:23 +0200

    Revert "Build: Add Custom Toolchain Support"
    This reverts commit b7424adf7f8ed43086ba1b8f3bfc47d16f647fa9.
    
    Change-Id: Ic3ccc051e8fe9725c1c502b8a81c3b2d950bb563

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -182,15 +182,7 @@ ifneq ($(USE_CCACHE),)
     ccache := $(strip $(wildcard $(ccache)))
 endif
 
-ifneq ($(TARGET_GCC_VERSION_ARM),)
-  ifeq ($(HOST_OS),darwin)
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
-  else
-    KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-$(TARGET_GCC_VERSION_ARM)/bin/arm-eabi-"
-  endif
-else
-  KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
-endif
+KERNEL_CROSS_COMPILE := CROSS_COMPILE="$(ccache) $(KERNEL_TOOLCHAIN_PATH)"
 ccache =
 
 define mv-modules
