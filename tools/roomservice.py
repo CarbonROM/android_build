1421a277b3f00bb6b3d3728402e97e1e59648c0d
STELIX <ssspinni@gmail.com>
2015-08-31 22:14:14 +0200

    Revert "Build - CarbonTEAM"
    This reverts commit 287c1c861392037fca1cfb73d665cb496ca290fc.

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -43,7 +43,7 @@ default_team_rem = "carbon"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "CarbonTEAM"
+android_team = "CarbonROM"
 
 
 def check_repo_exists(git_data):
