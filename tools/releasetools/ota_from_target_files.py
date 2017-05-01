409cea447967b7c956d2121452620dd7d30c76ad
Christian Oder <myself5@carbonrom.org>
2017-05-01 22:30:15 +0200

    AB Tools: Handle password encrypted keys
    Change-Id: I7c9b0198855a4b630c52b8552e904f312f09c4ce

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -198,6 +198,7 @@ OPTIONS.gen_verify = False
 OPTIONS.log_diff = None
 OPTIONS.payload_signer = None
 OPTIONS.payload_signer_args = []
+OPTIONS.key_passwords = []
 
 def MostPopularKey(d, default):
   """Given a dict, return the key corresponding to the largest
@@ -468,8 +469,7 @@ def CopyPartitionFiles(itemset, input_zip, output_zip=None, substitute=None):
 
 
 def SignOutput(temp_zip_name, output_zip_name):
-  key_passwords = common.GetKeyPasswords([OPTIONS.package_key])
-  pw = key_passwords[OPTIONS.package_key]
+  pw = OPTIONS.key_passwords[OPTIONS.package_key]
 
   common.SignFile(temp_zip_name, output_zip_name, OPTIONS.package_key, pw,
                   whole_file=True)
@@ -1339,7 +1339,25 @@ def WriteABOTAPackageWithBrilloScript(target_file, output_file,
     cmd.extend(["-out", rsa_key])
     p1 = common.Run(cmd, stdout=subprocess.PIPE)
     p1.wait()
-    assert p1.returncode == 0, "openssl pkcs8 failed"
+    if not p1.returncode == 0:
+      # this might be a decrypted key
+      pw = OPTIONS.key_passwords[OPTIONS.package_key]
+      # We don't want an verbose output to print the password
+      # Disable that and restore later
+      tmpverbose = OPTIONS.verbose
+      OPTIONS.verbose = False
+      cmd = ["openssl", "pkcs8",
+           "-in", OPTIONS.package_key + OPTIONS.private_key_suffix,
+           "-inform", "DER", "-passin", "pass:" + pw]
+      rsa_key = common.MakeTempFile(prefix="key-", suffix=".key")
+      cmd.extend(["-out", rsa_key])
+      p1 = common.Run(cmd, stdout=subprocess.PIPE)
+      if tmpverbose:
+        print "--- trying to convert decrypted keys ---"
+      p1.wait()
+      # Restore former verbose settings
+      OPTIONS.verbose = tmpverbose
+      assert p1.returncode == 0, "openssl pkcs8 failed"
 
   # Stage the output zip package for package signing.
   temp_zip_file = tempfile.NamedTemporaryFile()
@@ -2185,6 +2203,15 @@ def main(argv):
   OPTIONS.info_dict = common.LoadInfoDict(input_zip)
   common.ZipClose(input_zip)
 
+  # Use the default key to sign the package if not specified with package_key.
+  if not OPTIONS.no_signing:
+    if OPTIONS.package_key is None:
+      OPTIONS.package_key = OPTIONS.info_dict.get(
+          "default_system_dev_certificate",
+          "build/target/product/security/testkey")
+    # Get signing keys
+    OPTIONS.key_passwords = common.GetKeyPasswords([OPTIONS.package_key])
+
   ab_update = OPTIONS.info_dict.get("ab_update") == "true"
 
   if ab_update:
@@ -2244,13 +2271,6 @@ def main(argv):
   if OPTIONS.info_dict.get("no_recovery") == "true":
     raise common.ExternalError(
         "--- target build has specified no recovery ---")
-
-  # Use the default key to sign the package if not specified with package_key.
-  if not OPTIONS.no_signing:
-    if OPTIONS.package_key is None:
-      OPTIONS.package_key = OPTIONS.info_dict.get(
-          "default_system_dev_certificate",
-          "build/target/product/security/testkey")
 
   # Set up the output zip. Create a temporary zip file if signing is needed.
   if OPTIONS.no_signing:
