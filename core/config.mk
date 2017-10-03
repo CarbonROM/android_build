7de8a2c3a7a18b8b95d7a0847cc12d8e62693d66
XiNGRZ <chenxingyu92@gmail.com>
2017-10-04 00:13:07 +0800

    core: Prefix and build bison host executable if needed
    This patch prefixed bison with HOST_OUT_EXECUTABLES to ensure using our
    `bison` built from source. It also added bison to be a depencency of %.y
    and %.yy tasks so that it will be built if needed without any manual
    `mka bison`.
    
    Change-Id: Ic207124965c704b3f350e96b58d8e1f2683c2601

--- core/config.mk
+++ core/config.mk
@@ -521,7 +521,7 @@ LEX := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/flex/flex-2.5.39
 # BISON_PKGDATADIR.
 BISON_PKGDATADIR := $(PWD)/external/bison/data
 ifeq ($(USE_HOST_BISON),yes)
-BISON := bison
+BISON := $(HOST_OUT_EXECUTABLES)/bison
 else
 BISON := prebuilts/misc/$(BUILD_OS)-$(HOST_PREBUILT_ARCH)/bison/bison
 endif
